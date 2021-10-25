fa0f77ec9a4f66d8921bab40f6827baa1a6e18d9
Lokesh Mohanty <lokesh1197@gmail.com>
2021-10-25 16:10:44 +0530

    straight: move straight load config to init.el
    Move straight load script to init.el and install org before using
    org-babel inorder to use org from straight instead of built-in.
    
    Also replace all :straight nil with :straight (:type built-in) as per docs

--- init.el
+++ init.el
@@ -1,8 +1,35 @@
+;; Use straight.el instead of package.el
+(setq package-enable-at-startup nil)
+
+;; Bootstrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Always use straight to install on systems other than Linux
+(setq straight-use-package-by-default t)
+
+;; Use straight.el for use-package expressions
+(straight-use-package 'use-package)
+
+(use-package org
+  :straight org-contrib)
+
 (org-babel-load-file
  (expand-file-name
   "config.org"
   user-emacs-directory))
 
-(setq custom-file (concat user-emacs-directory "custom.el"))
-(when (file-exists-p custom-file)
-      (load custom-file))
+;; Required if using package.el
+;; (setq custom-file (concat user-emacs-directory "custom.el"))
+;; (when (file-exists-p custom-file)
+;;   (load custom-file))
