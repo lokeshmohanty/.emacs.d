6e2d3911620316d8f60088b79a2ba64d15fbd8c4
Lokesh Mohanty <lokesh1197@gmail.com>
2021-11-06 23:22:02 +0530

    init: uncomment custom.el file for local file/dir variables

--- init.el
+++ init.el
@@ -45,6 +45,6 @@
   user-emacs-directory))
 
 ;; Required if using package.el
-;; (setq custom-file (concat user-emacs-directory "custom.el"))
-;; (when (file-exists-p custom-file)
-;;   (load custom-file))
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
