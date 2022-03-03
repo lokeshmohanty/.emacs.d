b7055c69b6b3f8ed416f2da2e23f7ba3a8573c64
Lokesh Mohanty <lokesh1197@gmail.com>
2022-03-03 12:22:14 +0530

    early-init: copy from rational emacs by systemcrafters

--- early-init.el
+++ early-init.el
@@ -1,3 +1,41 @@
-;; Use straight.el instead of package.el
+;;; early-init.el -- Runs before starting emacs -*- lexical-binding: t; -*-
+;;; Commentary:
+;;
+;; Decreases Emacs load time by not loading unneeded features
+;; Reference: https://github.com/SystemCrafters/rational-emacs/blob/master/early-init.el
+
+;;; Code:
+;; Increase the GC threshold for faster startup
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
+
+;; Prefer loading newest compiled .el file
+(setq load-prefer-newer noninteractive)
+
+;; Native compilation settings
+(when (featurep 'native-compile)
+  ;; Silence compiler warnings as they can be pretty disruptive
+  (setq native-comp-async-report-warnings-errors nil)
+
+  ;; Make native compilation happens asynchronously
+  (setq native-comp-deferred-compilation t)
+
+  ;; Set the right directory to store the native compilation cache
+  (add-to-list 'native-comp-eln-load-path (expand-file-name "eln-cache/" user-emacs-directory)))
+
+;; Don't use package.el, we'll use straight.el instead
 (setq package-enable-at-startup nil)
 
+;; Remove some unneeded UI elements (the user can turn back on anything they wish)
+(setq inhibit-startup-message t)
+(push '(tool-bar-lines . 0) default-frame-alist) ;; disable the toolbar
+(push '(menu-bar-lines . 0) default-frame-alist) ;; disalbe the menu bar
+(push '(vertical-scroll-bars) default-frame-alist) ;; disable scroll bars
+(push '(background-color . "#232635") default-frame-alist)
+(push '(foreground-color . "#FFFFFF") default-frame-alist)
+(push '(mouse-color . "white") default-frame-alist)
+
+;; Make the initial buffer load faster by setting its mode to fundamental-mode
+(setq initial-major-mode 'fundamental-mode)
+
+;;; early-init.el ends here
